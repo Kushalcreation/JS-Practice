253380410b727846606fef85ee4b18e3fa89532f
NAVEEN DAS <107771166+dasnaveen25@users.noreply.github.com>
2026-01-02 01:33:34 +0530

    read functions and practice

---
+++ 01_function.js
@@ -0,0 +1,31 @@
+// function sayMyName() {
+//   console.log("Hello");
+//   console.log("Kushal");
+// }
+
+// sayMyName();
+
+const accountId = 66908;
+let accountEmail = "kushal@gmail.com";
+
+var accountPassword = "09987";
+/* 
+var is part of js in old time
+ but now it is removed and prefer not to be use 
+ because of issue in block scope and functional scope. 
+*/
+
+accountCity = "Raipur"; // Its also word, but not a good way.....
+
+//accountId = 000111  // a const keyword value's cant be changed once declared
+
+accountEmail = "jsbggs@gmail.com";
+accountPassword = "551515";
+accountCity = "Banglore";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+/*
+Thus, in js "Const" and "let" is usxed to declare variable
+ */
